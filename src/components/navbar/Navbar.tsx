ec1a699e858ad22827021f02a98432975af86aa4
sadness_ojisan <sadness.ojisan@gmail.com>
2019-02-20 01:22:27 +0900

    update: navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
+import Stars from "../../img/stars.gif";
+
+// TODO: should use styled-components
+const css = {
+  borderBottom: "1px solid #fff",
+  background: `#000 url(${Stars}) top left`
+};
+
+const StyledNavbar = props => {
+  const { children } = props;
+  return <Navbar style={css}>{children}</Navbar>;
+};
+
+export default StyledNavbar;
