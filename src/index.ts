514cb95d8fb2c2dfcad6867260e471944a7f03a1
sadness_ojisan <sadness.ojisan@gmail.com>
2019-02-21 10:59:48 +0900

    update: ver0.0.4

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,25 @@ import Input from "./components/form/Input";
 import Label from "./components/form/Label";
 import MultiSelectList from "./components/form/MultiSelectList";
 import SelectList from "./components/form/SelectList";
+import H1 from "./components/text/H1";
+import H2 from "./components/text/H2";
+import H3 from "./components/text/H3";
+import H4 from "./components/text/H4";
+import H5 from "./components/text/H5";
+import H6 from "./components/text/H6";
 
-export { Button, Navbar, Form, Input, Label, MultiSelectList, SelectList };
+export {
+  Button,
+  Navbar,
+  Form,
+  Input,
+  Label,
+  MultiSelectList,
+  SelectList,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6
+};
