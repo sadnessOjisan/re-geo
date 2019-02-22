6971aa64085f088094dd81baea08be945c399017
sadness_ojisan <sadness.ojisan@gmail.com>
2019-02-22 10:30:28 +0900

    update: paragraph example

--- src/components/text/Text.stories.tsx
+++ src/components/text/Text.stories.tsx
@@ -6,6 +6,7 @@ import H3 from "./H3";
 import H4 from "./H4";
 import H5 from "./H5";
 import H6 from "./H6";
+import P from "./P";
 
 storiesOf("Text", module)
   .add("h1", () => <H1>heyyhey</H1>)
@@ -13,4 +14,6 @@ storiesOf("Text", module)
   .add("H3", () => <H3>heyyhey</H3>)
   .add("H4", () => <H4>heyyhey</H4>)
   .add("H5", () => <H5>heyyhey</H5>)
-  .add("H6", () => <H6>heyyhey</H6>);
+  .add("H6", () => <H6>heyyhey</H6>)
+  .add("P", () => <P>heyyhey</P>)
+  .add("color P", () => <P color="blue">heyyhey</P>);
