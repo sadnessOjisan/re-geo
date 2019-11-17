e00762436f49011b30d8a2b85767c3c8177eac6b
sadnessOjisan <sadness.ojisan@gmail.com>
2019-11-18 03:14:56 +0900

    fix show image (#5)
    * fix image type
    
    * delete index.js
    
    * update: version

--- src/img/Image.stories.tsx
+++ src/img/Image.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
-import Image from "./";
+import Image from "./index";
 
 const text = (name: string) => `  
   ### what
