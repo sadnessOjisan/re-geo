eb8bef3fe25141ff8281535db94155d10ac148c0
sadness_ojisan <sadness.ojisan@gmail.com>
2020-05-05 00:40:09 +0900

    chore: all format

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "build.js",
-    libraryTarget: "commonjs2"
+    libraryTarget: "commonjs2",
   },
   module: {
     rules: [
@@ -16,25 +16,28 @@ module.exports = {
         test: /\.(js|ts|tsx)$/,
         use: [
           {
-            loader: "babel-loader"
-          }
-        ]
+            loader: "babel-loader",
+          },
+        ],
       },
       {
         test: /\.css/,
-        use: ["style-loader", { loader: "css-loader", options: { url: false } }]
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { url: false } },
+        ],
       },
       {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
-            loader: "url-loader"
-          }
-        ]
-      }
-    ]
+            loader: "url-loader",
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [".js", ".ts", ".tsx", ".css", ".gif", "jpg", "png"]
-  }
+    extensions: [".js", ".ts", ".tsx", ".css", ".gif", "jpg", "png"],
+  },
 };
