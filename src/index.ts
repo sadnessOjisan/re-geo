fc9fef5cb90a8c854a3090d409ecc535b799410e
sadnessOjisan <sadness.ojisan@gmail.com>
2020-09-27 23:46:31 +0900

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,25 @@
-import Button from "./components/button/Button";
-import H1 from "./components/text/H1";
-import H2 from "./components/text/H2";
-import H3 from "./components/text/H3";
-import H4 from "./components/text/H4";
-import H5 from "./components/text/H5";
-import H6 from "./components/text/H6";
-import Badge from "./components/misc/Badge";
-import Pager from "./components/misc/Pager";
-import ProgressBar from "./components/misc/ProgressBar";
-import Tabs from "./components/misc/Tabs";
-import Image from "./img/index";
-import Background from "./components/background/Background";
-import Font from "./components/font/Font";
-import GradationText from "./components/gradationText/GradationText";
-import Marquee from "./components/marquee/Marquee";
-import OutlineText from "./components/outlineText/OutlineText";
-import Rotator from "./components/rotator/Rotator";
-import Zoom from "./components/zoom/Zoom";
-import Center from "./components/center/Center";
-import Blink from "./components/blink/Blink";
-import Table from "./components/table/Table";
-import Alerts from "./components/misc/Alerts";
-import COLOR from "./constants/color";
-import FontStyle from "./constants/font";
-
-export {
-  Button,
-  H1,
-  H2,
-  H3,
-  H4,
-  H5,
-  H6,
-  Badge,
-  Pager,
-  ProgressBar,
-  Tabs,
-  Image,
-  Background,
-  Font,
-  GradationText,
-  Marquee,
-  OutlineText,
-  Rotator,
-  Zoom,
-  Center,
-  Blink,
-  Table,
-  Alerts,
-  COLOR,
-  FontStyle,
-};
+export * from "./components/button/Button";
+export * from "./components/text/H1";
+export * from "./components/text/H2";
+export * from "./components/text/H3";
+export * from "./components/text/H4";
+export * from "./components/text/H5";
+export * from "./components/text/H6";
+export * from "./components/misc/Badge";
+export * from "./components/misc/Pager";
+export * from "./components/misc/ProgressBar";
+export * from "./components/misc/Tabs";
+export * from "./img/index";
+export * from "./components/background/Background";
+export * from "./components/font/Font";
+export * from "./components/gradationText/GradationText";
+export * from "./components/marquee/Marquee";
+export * from "./components/outlineText/OutlineText";
+export * from "./components/rotator/Rotator";
+export * from "./components/zoom/Zoom";
+export * from "./components/center/Center";
+export * from "./components/blink/Blink";
+export * from "./components/table/Table";
+export * from "./components/misc/Alerts";
+export * from "./constants/color";
+export * from "./constants/font";
