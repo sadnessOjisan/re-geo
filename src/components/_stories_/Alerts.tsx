bebf4223d315e32fbf801b7ba93920530fd22990
sadnessOjisan <sadness.ojisan@gmail.com>
2020-09-29 05:14:51 +0900

    add alert component

--- src/components/_stories_/Alerts.tsx
+++ src/components/_stories_/Alerts.tsx
@@ -5,11 +5,22 @@ export interface Props {
   type: string;
 }
 
-export const AlertsStory: React.FC<Props> = () => {
+export const AlertC: React.FC<Props> = () => {
+  return <Alerts type="alert">test</Alerts>;
+};
+
+export const ErrorC: React.FC<Props> = () => {
+  return <Alerts type="error">test</Alerts>;
+};
+
+export const SuccessC: React.FC<Props> = () => {
   return <Alerts type="success">test</Alerts>;
 };
 
+export const InfoC: React.FC<Props> = () => {
+  return <Alerts type="info">test</Alerts>;
+};
+
 export default {
-  title: "Components/AlertsStory",
-  component: AlertsStory,
+  title: "Components/Alerts",
 };
