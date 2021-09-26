7f4691fbd828a135de13560fe95d169ff6d0d7af
Yuta Ide <sadness.ojisan@gmail.com>
2021-09-27 03:01:03 +0900

    Show image component (#40)
    * show comunity image
    
    * Add declaration file

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,11 +1,12 @@
 import { VFC } from "react";
 
-import { Blink } from "../../lib/src";
+import { Blink, Community } from "../../lib/src";
 
 const App: VFC = () => {
   return (
     <div className="App">
       <Blink text="hello" speed="slow" style={{ animationDuration: "0.01s" }} />
+      <img src={Community}></img>
     </div>
   );
 };
