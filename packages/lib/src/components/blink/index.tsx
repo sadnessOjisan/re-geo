b38e61a9786f7e8280a71a901c92e0afa0db19f4
sadness_ojisan <sadness.ojisan@gmail.com>
2021-09-27 02:33:09 +0900

    blink

--- packages/lib/src/components/blink/index.tsx
+++ packages/lib/src/components/blink/index.tsx
@@ -1,8 +1,25 @@
+import { css, keyframes } from "@stitches/react";
 import { VFC } from "react";
 
 type Props = {
   text: string;
 };
+
+const blinker = keyframes({
+  "0%": {
+    opacity: 0,
+  },
+  "75%": {
+    opacity: 1,
+  },
+});
+
+const styles = {
+  wrapper: css({
+    animation: `${blinker} 0.15s step-end infinite`,
+  }),
+};
+
 export const Blink: VFC<Props> = (props) => {
-  return <div>{props.text}</div>;
+  return <div className={styles.wrapper()}>{props.text}</div>;
 };
