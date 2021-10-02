b789307b2bf6ee769824089dadf25c4b85b238c6
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-02 16:00:27 +0900

    crazy storybook

--- packages/lp/src/pages/index.tsx
+++ packages/lp/src/pages/index.tsx
@@ -1,11 +1,41 @@
 import { Blink } from "re-geo";
+import {
+  Background,
+  ClassicMarquee,
+  Headline,
+  Rotator,
+  Text,
+  Zoom,
+} from "re-geo/src";
 import React, { VFC } from "react";
 
 const IndexPage: VFC = () => {
   return (
-    <main>
-      <Blink>aa</Blink> hello world
-    </main>
+    <div>
+      <Background pattern="microfab">
+        <Headline level={1}>re-geo</Headline>
+        <Text>npm i re-geo</Text>
+        <section>
+          <Headline level={2}>Component catalog</Headline>
+          <ClassicMarquee>
+            <a>storybook</a>
+          </ClassicMarquee>
+          <Zoom>
+            <a>storybook</a>
+          </Zoom>
+          <Rotator>
+            <a>storybook</a>
+          </Rotator>
+          <ClassicMarquee>
+            <Rotator>
+              <Zoom>
+                <a>storybook</a>
+              </Zoom>
+            </Rotator>
+          </ClassicMarquee>
+        </section>
+      </Background>
+    </div>
   );
 };
 
