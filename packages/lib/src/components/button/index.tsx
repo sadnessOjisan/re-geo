15aecb60cd5384068a442224e21e20779dfe7a18
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-02 19:07:06 +0900

    Add style

---
+++ packages/lib/src/components/button/index.tsx
@@ -0,0 +1,123 @@
+import { FC } from "react";
+
+import { Rainbow } from "../..";
+import {
+  BLACK,
+  CYAN,
+  GRAY,
+  GREEN,
+  RED,
+  WHITE,
+  YELLOW,
+} from "../../const/internal/color";
+import { assertNever } from "../../util/internal/assert-never";
+
+type Props = {
+  type?:
+    | "default"
+    | "primary"
+    | "info"
+    | "success"
+    | "warning"
+    | "danger"
+    | "inverse";
+  size?: "normal" | "large" | "small";
+  isDisable?: boolean;
+  __unsafe__style?: React.CSSProperties;
+};
+
+const DEFAULT_DISABLE = false;
+
+export const Button: FC<Props> = ({
+  children,
+  type,
+  size,
+  isDisable,
+  __unsafe__style,
+}) => {
+  let skinStyle: React.CSSProperties = {};
+  switch (type) {
+    case "default":
+    case undefined:
+      skinStyle = {
+        color: GRAY,
+      };
+      break;
+    case "primary":
+      skinStyle = {
+        color: WHITE,
+        background: `#000 url(${Rainbow}) top left`,
+      };
+      break;
+    case "info":
+      skinStyle = {
+        color: WHITE,
+        backgroundColor: CYAN,
+      };
+      break;
+    case "success":
+      skinStyle = {
+        color: WHITE,
+        backgroundColor: GREEN,
+      };
+      break;
+    case "warning":
+      skinStyle = {
+        color: WHITE,
+        backgroundColor: YELLOW,
+      };
+      break;
+    case "danger":
+      skinStyle = {
+        color: WHITE,
+        backgroundColor: RED,
+      };
+      break;
+    case "inverse":
+      skinStyle = {
+        color: WHITE,
+        backgroundColor: BLACK,
+      };
+      break;
+    default:
+      assertNever(type);
+  }
+
+  let sizeStyle: React.CSSProperties = {};
+  switch (size) {
+    case "normal":
+    case undefined:
+      sizeStyle = {
+        padding: "4px 12px",
+        fontSize: 16,
+      };
+      break;
+    case "small":
+      sizeStyle = {
+        padding: "2px 8px",
+        fontSize: 12,
+      };
+      break;
+    case "large":
+      sizeStyle = {
+        padding: "12px 20px",
+        fontSize: 20,
+      };
+      break;
+    default:
+      assertNever(size);
+  }
+  return (
+    <button
+      disabled={isDisable === undefined ? DEFAULT_DISABLE : isDisable}
+      style={{
+        ...skinStyle,
+        ...sizeStyle,
+        ...__unsafe__style,
+        border: "6px ridge #bbb",
+      }}
+    >
+      {children}
+    </button>
+  );
+};
