68a37d87a60c79c85f9a982072f637c720f87936
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-02 23:25:10 +0900

    add props

--- packages/lib/src/components/marquee/classic-marquee.tsx
+++ packages/lib/src/components/marquee/classic-marquee.tsx
@@ -1,15 +1,46 @@
 import { FC } from "react";
 
+// FYI: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/marquee
 type Props = {
   behavior?: "scroll" | "slide" | "alternate";
   direction?: "up" | "down" | "left" | "right";
+  bgcolor?: string;
+  height?: string;
+  hspace?: string;
+  loop?: string;
+  scrollamout?: string;
+  scrolldelay?: string;
+  truespeed?: string;
+  vspace?: string;
+  width?: string;
+  __unsafe__style?: React.CSSProperties;
+  // TODO: impl event handler
+  // onbounce: (e: any) => void;
+  // onfinish: (e: any) => void;
+  // onstart: (e: any) => void;
 };
 
+/**
+ * Now marquee is deprecated and tsc raise error if you use native marquee.
+ * That's why we provide classic marquee wrapper to avoid compile error in your env.
+ */
 export const ClassicMarquee: FC<Props> = (props) => {
   return (
     // eslint-disable-next-line
     // @ts-ignore
-    <marquee behavior={props.behavior} direction={props.direction}>
+    <marquee
+      behavior={props.behavior}
+      direction={props.direction}
+      width={props.width}
+      height={props.height}
+      scrolldelay={props.scrollamout}
+      scrollamount={props.scrollamout}
+      loop={props.loop}
+      bgcolor={props.bgcolor}
+      vspace={props.vspace}
+      hspace={props.hspace}
+      style={props.__unsafe__style}
+    >
       {props.children}
       {/* eslint-disable-next-line */}
       {/* @ts-ignore */}
