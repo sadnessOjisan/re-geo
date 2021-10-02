997b7d930afc87450c627c383c0ff75c1f445930
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-02 23:59:54 +0900

    fix type

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -5,7 +5,7 @@ import { Blink, Community } from "../../lib/src";
 const App: VFC = () => {
   return (
     <div className="App">
-      <Blink speed="slow" style={{ animationDuration: "0.01s" }}>
+      <Blink __unsafe__style={{ animationDuration: "0.01s" }}>
         hello world
       </Blink>
       <img src={Community}></img>
