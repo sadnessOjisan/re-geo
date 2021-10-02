c14522a049a1296ff5e86cae5193344b7cf13b6a
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-02 22:22:57 +0900

    export all images

--- packages/lib/src/assets/index.stories.tsx
+++ packages/lib/src/assets/index.stories.tsx
@@ -11,7 +11,34 @@ import {
   Divider1,
   Divider2,
   Divider3,
+  Drudgesiren,
+  Emailme,
+  Flames,
+  Funky,
+  Geocities,
+  GlyphiconsHalflingsWhite,
+  Hacker,
+  Heart,
+  Hot,
+  IELogo,
+  Mailkitten,
+  Mchammer,
+  Microfab,
+  New,
+  New2,
+  Noframes,
+  Notepad,
+  NSLogo,
+  Progress,
+  Rainbow,
   SevenUpspot,
+  SignIn,
+  Spinningearth,
+  Stars,
+  Underconstruction,
+  Wabwalk,
+  Webtrips,
+  Yahooweek,
 } from ".";
 
 export default {
@@ -53,3 +80,87 @@ Divider2Image.args = { href: Divider2 };
 
 export const Divider3Image = Template.bind({});
 Divider3Image.args = { href: Divider3 };
+
+export const DrudgesirenImage = Template.bind({});
+DrudgesirenImage.args = { href: Drudgesiren };
+
+export const EmailmeImage = Template.bind({});
+EmailmeImage.args = { href: Emailme };
+
+export const FlamesImage = Template.bind({});
+FlamesImage.args = { href: Flames };
+
+export const FunkyImage = Template.bind({});
+FunkyImage.args = { href: Funky };
+
+export const GeocitiesImage = Template.bind({});
+GeocitiesImage.args = { href: Geocities };
+
+export const GlyphiconsHalflingsWhiteImage = Template.bind({});
+GlyphiconsHalflingsWhiteImage.args = { href: GlyphiconsHalflingsWhite };
+
+export const GlyphiconsHalflingsImage = Template.bind({});
+GlyphiconsHalflingsImage.args = { href: GlyphiconsHalflings };
+
+export const HackerImage = Template.bind({});
+HackerImage.args = { href: Hacker };
+
+export const HeartImage = Template.bind({});
+HeartImage.args = { href: Heart };
+
+export const HotImage = Template.bind({});
+HotImage.args = { href: Hot };
+
+export const IELogoImage = Template.bind({});
+IELogoImage.args = { href: IELogo };
+
+export const MailkittenImage = Template.bind({});
+MailkittenImage.args = { href: Mailkitten };
+
+export const MchammerImage = Template.bind({});
+MchammerImage.args = { href: Mchammer };
+
+export const MicrofabImage = Template.bind({});
+MicrofabImage.args = { href: Microfab };
+
+export const NewImage = Template.bind({});
+NewImage.args = { href: New };
+
+export const New2Image = Template.bind({});
+New2Image.args = { href: New2 };
+
+export const NoframesImage = Template.bind({});
+NoframesImage.args = { href: Noframes };
+
+export const NotepadImage = Template.bind({});
+NotepadImage.args = { href: Notepad };
+
+export const NSLogoImage = Template.bind({});
+NSLogoImage.args = { href: NSLogo };
+
+export const ProgressImage = Template.bind({});
+ProgressImage.args = { href: Progress };
+
+export const RainbowImage = Template.bind({});
+RainbowImage.args = { href: Rainbow };
+
+export const SignInImage = Template.bind({});
+SignInImage.args = { href: SignIn };
+
+export const SpinningearthImage = Template.bind({});
+SpinningearthImage.args = { href: Spinningearth };
+
+export const StarsImage = Template.bind({});
+StarsImage.args = { href: Stars };
+
+export const UnderconstructionImage = Template.bind({});
+UnderconstructionImage.args = { href: Underconstruction };
+
+export const WabwalkImage = Template.bind({});
+WabwalkImage.args = { href: Wabwalk };
+
+export const WebtripsImage = Template.bind({});
+WebtripsImage.args = { href: Webtrips };
+
+export const YahooweekImage = Template.bind({});
+YahooweekImage.args = { href: Yahooweek };
